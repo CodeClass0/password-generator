6304514f32c001ad27f52a1dc183fdd716dad34a
C A <chadandari@outlook.com>
2023-12-14 22:05:41 -0500

    Added function structure for when the button is clicked.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+function pwLength(){
+
+}
+
+function charType(){
+
+}
+
+function recapParams(){
+
+}
 // Write password to the #password input
 function writePassword() {
   password = generatePw();
@@ -10,5 +24,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function(event){
-
+  pwLength(); //Prompts user for the length of the password and validates their input. 
+  charType(); //Promots the user for numbers, letters, and special characters. Validates input, and populates array with possible characters.
+  recapParams(); //Lets the user know the final parameters of their password.
+  writePassword(); //Writes password to the screen.
 });
+
